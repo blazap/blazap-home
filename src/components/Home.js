ae99ecc457718182897e165d79c86191bff33955
itroeix <deividgeorge2017@gmail.com>
2022-10-17 22:43:54 +0200

    New UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,14 +14,25 @@ const Home = () => {
         </div>   
     */
     return (
-        <header className="bg-image">
-        <h1>Blazap - Store of webapps</h1>
-        <h2>And you can convert a webapp to a normal app!</h2>
-        <button class="formbtn" onClick={() => window.location.href = process.env.REACT_APP_WEBAPP_REDIRECT}>Launch on the web</button>
-        <button class="formbtn" onClick={() => window.location.href = process.env.REACT_APP_DOWNLOADS_REDIRECT}>Downloads (not available)</button><br />
-        <a class="convert" href={process.env.REACT_APP_GHREPO_REDIRECT}>view on GitHub</a>
+        <section class="container">
+            <div class="left-half">
+            <article>
+                <h1>Blazap - Store of webapps</h1>
+                <h2>And you can convert a webapp to a normal app!</h2>
+                <button class="formbtn" onClick={() => window.location.href = process.env.REACT_APP_WEBAPP_REDIRECT}>Launch on the web</button>
+                <button class="formbtn" onClick={() => window.location.href = process.env.REACT_APP_DOWNLOADS_REDIRECT}>Downloads (not available)</button><br />
+                <a class="convert" href={process.env.REACT_APP_GHREPO_REDIRECT}>view on GitHub</a>
+                </article>            
+                </div>
+            <div class="right-half">
+                <article>
+                    <img src="https://cdn.discordapp.com/attachments/1017260992650027058/1031664889468555324/sincolor.png" />
+                </article>
+            </div>
+        </section>
 
-    </header>
+
+
     )
 }
 
